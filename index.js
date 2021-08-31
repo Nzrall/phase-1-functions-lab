3786d321194048d2818d87a8a231ca3748d9338f
Joshua <wilson.joshua@outlook.com>
2021-08-31 09:55:01 -0600

    commit tree 9ce951fd17de19af53e98f4997b7c6d548eb6032

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+let headQuarters = 42;
+let blockInFeet = 264;
+
+
+function distanceFromHqInBlocks(pickUpLocation) {
+    
+    return Math.abs (pickUpLocation - headQuarters)
+}
+function distanceFromHqInFeet(startingBlock) {
+    let distanceTravelledInBlocks = distanceFromHqInBlocks(startingBlock);
+    return distanceTravelledInBlocks * blockInFeet;
+}
+let distanceTravelledInFeet = (startingBlock, destinationBlock) => {
+    let distanceInBlocks = Math.abs (startingBlock - destinationBlock)
+    return distanceInBlocks * blockInFeet
+}
+let calculatesFarePrice = (startingBlock, destinationBlock) => {
+let feetTravelled = distanceTravelledInFeet (startingBlock,destinationBlock);
+if (feetTravelled <= 400) {
+    return 0;
+} else if (feetTravelled >400 && feetTravelled <= 2000) {
+    return (feetTravelled - 400) * 0.02;
+} else if (feetTravelled > 2000 && feetTravelled <= 2500) {
+    return 25;
+} else {
+    return 'cannot travel that far'
+}
+
+}
+
+
